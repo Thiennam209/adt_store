768ad72f31423e85660d2fc6db0c66a3d2900e0f
nam <thiennam209@gmail.com>
2022-10-27 16:19:54 +0700

    first commit

---
+++ functions/SignalRFunctions.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.EventGrid.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace SignalRFunctions
+{
+    public static class SignalRFunctions
+    {
+        public static string storeid;
+        public static string timeInterval;
+        public static string BestSellingProductName;
+        public static int BestSellingProductQuantity;
+        public static string LeastSellingProductName;
+        public static int LeastSellingProductQuantity;
+        public static int CustomerQuantityPos1;
+        public static int CustomerQuantityPos2;
+        public static int CustomerQuantityPos3;
+        public static int CustomerQuantityPos4;
+
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo GetSignalRInfo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req,
+            [SignalRConnectionInfo(HubName = "dttelemetry")] SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
+
+        [FunctionName("broadcast")]
+        public static Task SendMessage(
+            [EventGridTrigger] EventGridEvent eventGridEvent,
+            [SignalR(HubName = "dttelemetry")] IAsyncCollector<SignalRMessage> signalRMessages,
+            ILogger log)
+        {
+            JObject eventGridData = (JObject)JsonConvert.DeserializeObject(eventGridEvent.Data.ToString());
+            if (eventGridEvent.EventType.Contains("telemetry"))
+            {
+                var data = eventGridData.SelectToken("data");
+
+                var telemetryMessage = new Dictionary<object, object>();
+                foreach (JProperty property in data.Children())
+                {
+                    log.LogInformation(property.Name + " - " + property.Value);
+                    telemetryMessage.Add(property.Name, property.Value);
+                }
+                return signalRMessages.AddAsync(
+                new SignalRMessage
+                {
+                    Target = "TelemetryMessage",
+                    Arguments = new[] { telemetryMessage }
+                });
+            }
+            else
+            {
+                try
+                {
+                    storeid = eventGridEvent.Subject;
+                    
+                    var data = eventGridData.SelectToken("data");
+                    var patch = data.SelectToken("patch");
+
+                    var property = new Dictionary<object, object>
+                    {
+                        {"storeid", storeid }
+                    };
+                    return signalRMessages.AddAsync(
+                        new SignalRMessage
+                        {
+                            Target = "PropertyMessage",
+                            Arguments = new[] { property }
+                        });
+                }
+                catch (Exception e)
+                {
+                    log.LogInformation(e.Message);
+                    return null;
+                }
+            }
+
+        }
+    }
+}
---
+++ functions/telemetryfunction.cs
@@ -0,0 +1,98 @@
+using Azure;
+using Azure.Core.Pipeline;
+using Azure.DigitalTwins.Core;
+using Azure.Identity;
+using Microsoft.Azure.EventGrid.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+
+namespace My.Function
+{
+    // This class processes telemetry events from IoT Hub, reads temperature of a device
+    // and sets the "Temperature" property of the device with the value of the telemetry.
+    public class telemetryfunction
+    {
+        private static readonly HttpClient httpClient = new HttpClient();
+        private static string adtServiceUrl = Environment.GetEnvironmentVariable("ADT_SERVICE_URL");
+
+        [FunctionName("telemetryfunction")]
+        public async void Run([EventGridTrigger] EventGridEvent eventGridEvent, ILogger log)
+        {
+            try
+            {
+                // After this is deployed, you need to turn the Managed Identity Status to "On",
+                // Grab Object Id of the function and assigned "Azure Digital Twins Owner (Preview)" role
+                // to this function identity in order for this function to be authorized on ADT APIs.
+                //Authenticate with Digital Twins
+                var credentials = new DefaultAzureCredential();
+                log.LogInformation(credentials.ToString());
+                DigitalTwinsClient client = new DigitalTwinsClient(
+                    new Uri(adtServiceUrl), credentials, new DigitalTwinsClientOptions
+                    { Transport = new HttpClientTransport(httpClient) });
+                log.LogInformation($"ADT service client connection created.");
+                
+                if (eventGridEvent != null && eventGridEvent.Data != null)
+                {
+                    JObject deviceMessage = (JObject)JsonConvert.DeserializeObject(eventGridEvent.Data.ToString());
+                    string deviceId = (string)deviceMessage["systemProperties"]["iothub-connection-device-id"];
+                    var ID = deviceMessage["body"]["storeid"];
+                    var TimeInterval = deviceMessage["body"]["TimeInterval"];
+                    var BestSellingProductName = deviceMessage["body"]["BestSellingProductName"];
+                    var BestSellingProductQuantity = deviceMessage["body"]["BestSellingProductQuantity"];
+                    var LeastSellingProductName = deviceMessage["body"]["LeastSellingProductName"];
+                    var LeastSellingProductQuantity = deviceMessage["body"]["LeastSellingProductQuantity"];
+                    var CustomerQuantityPos1 = deviceMessage["body"]["CustomerQuantityPos1"];
+                    var CustomerQuantityPos2 = deviceMessage["body"]["CustomerQuantityPos2"];
+                    var CustomerQuantityPos3 = deviceMessage["body"]["CustomerQuantityPos3"];
+                    var CustomerQuantityPos4 = deviceMessage["body"]["CustomerQuantityPos4"];
+
+                    log.LogInformation($"Device:{deviceId} Device Id is:{ID}");
+                    log.LogInformation($"Device:{deviceId} Time interval is:{TimeInterval}");
+                    log.LogInformation($"Device:{deviceId} BestSellingProductName is:{BestSellingProductName}");
+                    log.LogInformation($"Device:{deviceId} BestSellingProductQuantity is:{BestSellingProductQuantity}");
+                    log.LogInformation($"Device:{deviceId} LeastSellingProductName is:{LeastSellingProductName}");
+                    log.LogInformation($"Device:{deviceId} LeastSellingProductQuantity is:{LeastSellingProductQuantity}");
+                    log.LogInformation($"Device:{deviceId} CustomerQuantityPos1:{CustomerQuantityPos1}");
+                    log.LogInformation($"Device:{deviceId} CustomerQuantityPos2 is:{CustomerQuantityPos2}");
+                    log.LogInformation($"Device:{deviceId} CustomerQuantityPos3 is:{CustomerQuantityPos3}");
+                    log.LogInformation($"Device:{deviceId} CustomerQuantityPos4 is:{CustomerQuantityPos4}");
+                    var updateProperty = new JsonPatchDocument();
+                    var storeTelemetry = new Dictionary<string, Object>()
+                    {
+                        ["storeid"] = ID,
+                        ["TimeInterval"] = TimeInterval,
+                        ["BestSellingProductName"] = BestSellingProductName,
+                        ["BestSellingProductQuantity"] = BestSellingProductQuantity,
+                        ["LeastSellingProductName"] = LeastSellingProductName,
+                        ["LeastSellingProductQuantity"] = LeastSellingProductQuantity,
+                        ["CustomerQuantityPos1"] = CustomerQuantityPos1,
+                        ["CustomerQuantityPos2"] = CustomerQuantityPos2,
+                        ["CustomerQuantityPos3"] = CustomerQuantityPos3,
+                        ["CustomerQuantityPos4"] = CustomerQuantityPos4
+                    };
+                    updateProperty.AppendAdd("/storeid", ID.Value<string>());
+
+                    log.LogInformation(updateProperty.ToString());
+                    try
+                    {
+                        await client.PublishTelemetryAsync(deviceId, Guid.NewGuid().ToString(), JsonConvert.SerializeObject(storeTelemetry));
+                    }
+                    catch (Exception e)
+                    {
+                        log.LogInformation(e.Message);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                log.LogInformation(e.Message);
+            }
+        }
+    }
+}
